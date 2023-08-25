a90f06fcda7172abf9e1b4169f1aacd338961e68
Josep Valls <alfredvays@gmail.com>
2023-08-25 17:19:29 +0200

    AdvertisementItem for AdvertisementListScreen

--- app/src/main/java/com/jossidfactory/handwebber/screen/advertisement/list/AdvertisementsListScreen.kt
+++ app/src/main/java/com/jossidfactory/handwebber/screen/advertisement/list/AdvertisementsListScreen.kt
@@ -3,19 +3,36 @@ package com.jossidfactory.handwebber.screen.advertisement.list
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.jossidfactory.handwebber.BuildConfig
+import com.jossidfactory.handwebber.R
+import com.jossidfactory.handwebber.domain.advertisement.model.AdvertisementModel
 import org.koin.androidx.compose.koinViewModel
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -28,20 +45,24 @@ fun AdvertisementsListScreen(
     val state: AdvertisementsListState by advertisementsListViewModel.state.observeAsState(AdvertisementsListState())
 
     Scaffold(
-        modifier = Modifier.background(MaterialTheme.colorScheme.background)
+        modifier = Modifier.background(MaterialTheme.colorScheme.background),
+        topBar = {TopBarApp()}
     ) {
         LazyColumn(
             modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
         ) {
             items(state.advertisements) { advertisement ->
-                Text(
-                    text = advertisement.name,
-                    modifier = Modifier.clickable { /*onDetailClick.invoke(beer.id) */ },
-                    color = MaterialTheme.colorScheme.primary,
-                    fontWeight = FontWeight.Bold,
-                    style = MaterialTheme.typography.titleLarge
-                )
+                AdvertisementItem(advertisement = advertisement)
             }
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+//@Preview(showBackground = true)
+fun TopBarApp() {
+    TopAppBar(title = { Text(text = stringResource(id = R.string.app_name)) }, )
+}
+
+
